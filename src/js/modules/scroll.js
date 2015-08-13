9502e0e197fd1d70ee607e43aeb2f4d46b41b7d1
Sam Morris <mail@sammorrisdesign.co.uk>
2015-08-13 16:49:34 +0100

    Remove older stuff

---
+++ src/js/modules/scroll.js
@@ -0,0 +1,13 @@
+define([
+    'libs/jquery'
+], function(
+    jquery
+){
+    var scrollutils = {
+        init: function() {
+            console.log("init");
+        }
+    };
+
+    return scrollutils;
+});
