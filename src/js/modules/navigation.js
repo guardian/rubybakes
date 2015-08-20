ceb599a64c5b8b19d9d004ecceca7411486c721b
Sam Morris <mail@sammorrisdesign.co.uk>
2015-08-20 16:48:06 +0100

    Smooth scrolling to anchor points

--- src/js/modules/navigation.js
+++ src/js/modules/navigation.js
@@ -15,18 +15,33 @@ define([
             $(window).scroll(function() {
                 this.updateNavigation();
             }.bind(this));
+            $(".recipe-navigation__link").click(function(e) {
+                this.scrollToAnchor(e);
+            }.bind(this));
         },
 
         updateNavigation: function() {
             scrollTop = $(window).scrollTop();
             $(".navigation__point").each(function(index) {
-                if ($(this).offset().top > scrollTop) {
+                if ($(this).offset().top >= scrollTop) {
+                    console.log($(".navigation__point:eq(" + (index + 1) + ")").attr("id"));
                     $(".recipe-navigation__point__title").text($(this).data("navigation-title"));
                     $(".recipe-navigation__point__subtitle").text($(this).data("navigation-subtitle"));
                     $(".recipe-navigation__link").attr("href", "#" + $(".navigation__point:eq(" + (index + 1) + ")").attr("id"));
                     return false;
                 }
             });
+        },
+
+        scrollToAnchor: function(e) {
+            e.preventDefault();
+            var scrollTo = $(e.currentTarget.hash).offset().top;
+            if (e.currentTarget.hash !== "#intro") {
+                scrollTo -= $(".recipe__ingredients__label").height();
+            }
+            $("html, body").animate({
+                scrollTop: scrollTo
+            }, 'slow');
         }
     };
 
