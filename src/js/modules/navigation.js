16c369b6bbebbbf58c5af9709f26eb0933c98084
Sam Morris <mail@sammorrisdesign.co.uk>
2015-08-20 12:07:25 +0100

    Remove console logs

--- src/js/modules/navigation.js
+++ src/js/modules/navigation.js
@@ -21,10 +21,8 @@ define([
             scrollTop = $(window).scrollTop();
             $(".navigation__point").each(function(index) {
                 if ($(this).offset().top > scrollTop) {
-                    console.log(index);
                     $(".recipe-navigation__point__title").text($(this).data("navigation-title"));
                     $(".recipe-navigation__point__subtitle").text($(this).data("navigation-subtitle"));
-                    console.log($(".navigation__point")[2]);
                     $(".recipe-navigation__link").attr("href", "#" + $(".navigation__point:eq(" + (index + 1) + ")").attr("id"));
                     return false;
                 }
